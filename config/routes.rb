ac665236373d2faf5baee4e4d00fc3d188614e9c
Kyle Wilson <kydanielwilson@gmail.com>
2016-04-02 03:45:29 +0000

    trying to fix / route for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rottenpotatoes::Application.routes.draw do
   resources :movies do
     match "similar" => "movies#similar"
   end
-
+  
   # Sample resource route with options:
   #   resources :products do
   #     member do
@@ -51,7 +51,7 @@ Rottenpotatoes::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'movies#index'
 
   # See how all your routes lay out with "rake routes"
 
